1d565d690c665a816757020400be26416f116bdd
mubeen207 <mubeenulislam207@gmail.com>
2025-09-13 00:42:24 +0500

    Funtionality setting

--- OTP CHAKER/app.js
+++ OTP CHAKER/app.js
@@ -1,14 +1,10 @@
 let otpholder = 0;
 let otp_message = document.getElementById("otp-message");
 function sentotp() {
-  let num1 = Math.ceil(Math.random() * 6);
-  let num2 = Math.ceil(Math.random() * 6);
-  let num3 = Math.ceil(Math.random() * 6);
-  let num4 = Math.ceil(Math.random() * 6);
-  let num5 = Math.ceil(Math.random() * 6);
-  let num6 = Math.ceil(Math.random() * 6);
-
-  otp = num1.toString() + num2 + num3 + num4 + num5 + num6;
+  let otp = "";
+  for (let i = 0; i < 6; i++) {
+    otp = otp + Math.ceil(Math.random() * 9);
+  }
   otp_message.innerHTML = "Your OTP is " + otp;
   //   alert("Your OTP is " + otp);
   otpholder = otp;
@@ -18,12 +14,15 @@ function checkotp() {
   let Wrong = document.createElement("p");
   let body = document.body;
 
-  if (otpInput.value !== "") {
+  if (otpInput.value > 0 && otpholder <= 0) {
     body.appendChild(Wrong);
     Wrong.innerHTML = "Please Sent OTP First";
-  } else if (otpInput.value >= 0 && otpholder === 0) {
+  } else if (otpInput !== "" && otpholder <= 0) {
     body.appendChild(Wrong);
     Wrong.innerHTML = "Please Enter OTP First";
+  } else if (otpInput.value >= 0 && otpholder === 0) {
+    body.appendChild(Wrong);
+    Wrong.innerHTML = "Please Sent OTP First";
   } else if (otpholder === otpInput.value) {
     body.appendChild(Wrong);
     Wrong.innerHTML = "Welcome";
