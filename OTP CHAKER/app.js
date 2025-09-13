5c2cc95c605bc5ada1b65b616a0238b7c8692a3f
mubeen207 <mubeenulislam207@gmail.com>
2025-09-14 00:40:34 +0500

    timmer

--- OTP CHAKER/app.js
+++ OTP CHAKER/app.js
@@ -4,58 +4,48 @@ let otpInput = document.getElementById("otpInput");
 let second = 60;
 let otpholder = "";
 let otp_message = document.getElementById("otp-message");
-let otpInterval; // Declare otpInterval globally
+let otpInterval;
 
-// function sentotp() {
-//   generateOtp();
-
-//   // clearInterval(otpInterval);
-
-//   otpInterval = setInterval(generateOtp, 10000);
-// }
-
-// function generateOtp() {
-//   let otp = "";
-//   for (let i = 0; i < 6; i++) {
-//     otp += Math.floor(Math.random() * 10);
-//   }
-//   otp_message.innerHTML = "Your OTP is " + otp;
-//   otpholder = otp;
-// }
 function sentotp() {
   closetimer();
   let otp = "";
   for (let i = 0; i < 6; i++) {
-    otp = otp + Math.floor(Math.random() * 10);
+    otp += Math.floor(Math.random() * 10);
   }
   otp_message.innerHTML = "Your OTP is " + otp;
   otpholder = otp;
 
-  otpInterval = setInterval(function time() {
-    timer.innerHTML = "00 : " + second;
+  otpInterval = setInterval(function () {
+    timer.innerHTML = "00 : " + (second < 10 ? "0" + second : second);
     if (second > 0) {
       second--;
     } else {
       otpholder = "";
-      otp_message.innerHTML = "Your OTP is " + "Expired";
+      otp_message.innerHTML = "Your OTP is Expired";
       clearInterval(otpInterval);
-      second = 60;
+      timer.innerHTML = "00 : 00";
     }
   }, 1000);
 }
+
 function closetimer() {
   clearInterval(otpInterval);
   second = 60;
 }
+
 function checkotp() {
   if (otpholder === "") {
     Wrong.innerHTML = "Please Send OTP First";
+    Wrong.style.color = "black";
   } else if (otpInput.value === "") {
     Wrong.innerHTML = "Please Enter OTP First";
+    Wrong.style.color = "black";
   } else if (otpInput.value === otpholder) {
     Wrong.innerHTML = "Welcome";
     Wrong.style.color = "green";
     otpholder = "";
+    closetimer();
+    timer.innerHTML = "00 : 00";
   } else {
     Wrong.innerHTML = "OTP is wrong, Try Again";
     Wrong.style.color = "red";
