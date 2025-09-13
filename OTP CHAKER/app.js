18e12c2ae577f89f45c9f57d0f43d9e2442d49b2
MUBEEN207 <mubeenulislam207@gmail.com>
2025-09-13 05:54:04 -0700

    insertBefore

--- OTP CHAKER/app.js
+++ OTP CHAKER/app.js
@@ -1,5 +1,7 @@
 let Wrong = document.createElement("p");
-document.body.appendChild(Wrong);
+let otpInput = document.getElementById("otpInput");
+
+document.body.insertBefore(Wrong, otpInput);
 
 let otpholder = "";
 let otp_message = document.getElementById("otp-message");
@@ -7,22 +9,20 @@ let otp_message = document.getElementById("otp-message");
 function sentotp() {
   let otp = "";
   for (let i = 0; i < 6; i++) {
-    otp += Math.floor(Math.random() * 10); // 0–9
+    otp = otp + Math.floor(Math.random() * 10);
   }
   otp_message.innerHTML = "Your OTP is " + otp;
-  otpholder = otp; // string rakha
+  otpholder = otp;
 }
 
 function checkotp() {
-  let otpInput = document.getElementById("otpInput");
-
   if (otpholder === "") {
     Wrong.innerHTML = "Please Send OTP First";
   } else if (otpInput.value === "") {
     Wrong.innerHTML = "Please Enter OTP First";
   } else if (otpInput.value === otpholder) {
     Wrong.innerHTML = "Welcome";
-    otpholder = ""; // reset OTP after success
+    otpholder = ""; // reset
   } else {
     Wrong.innerHTML = "OTP is wrong, Try Again";
   }
