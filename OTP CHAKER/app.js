e1084205bd34faad3fcdfafeca0e1e91dd32c111
mubeen207 <mubeenulislam207@gmail.com>
2025-09-14 00:46:38 +0500

    input styling added

--- OTP CHAKER/app.js
+++ OTP CHAKER/app.js
@@ -5,8 +5,12 @@ let second = 60;
 let otpholder = "";
 let otp_message = document.getElementById("otp-message");
 let otpInterval;
+let count = 0;
 
 function sentotp() {
+  if (count > 0) {
+    otpInput.style.border = "0.5px solid black";
+  }
   closetimer();
   let otp = "";
   for (let i = 0; i < 6; i++) {
@@ -44,11 +48,17 @@ function checkotp() {
     Wrong.innerHTML = "Welcome";
     Wrong.style.color = "green";
     otpholder = "";
+    otpInput.style.border = "2px solid green";
+    count++;
     closetimer();
     timer.innerHTML = "00 : 00";
   } else {
     Wrong.innerHTML = "OTP is wrong, Try Again";
     Wrong.style.color = "red";
+    if (otpInput.value !== otpholder) {
+      otpInput.style.border = "2px solid red";
+      count++;
+    }
   }
   otpInput.value = "";
 }
