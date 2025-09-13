95fcda32e17fb211e5d4b1e68a2ccd4c7b0d6d28
MUBEEN207 <mubeenulislam207@gmail.com>
2025-09-13 06:46:50 -0700

    timmer added

--- OTP CHAKER/app.js
+++ OTP CHAKER/app.js
@@ -1,31 +1,44 @@
-let Wrong = document.createElement("p");
-let otpInput = document.getElementById("otpInput");
+window.onload = function () {
+  let Wrong = document.getElementById("otp");
+  let otpInput = document.getElementById("otpInput");
 
-document.body.childNodes[1].insertBefore(Wrong, otpInput);
+  let otpholder = "";
+  let otp_message = document.getElementById("otp-message");
 
-let otpholder = "";
-let otp_message = document.getElementById("otp-message");
+  function sentotp() {
+    generateOtp();
 
-function sentotp() {
-  let otp = "";
-  for (let i = 0; i < 6; i++) {
-    otp = otp + Math.floor(Math.random() * 10);
-  }
-  otp_message.innerHTML = "Your OTP is " + otp;
-  otpholder = otp;
-}
+    // clearInterval(otpInterval);
 
-function checkotp() {
-  if (otpholder === "") {
-    Wrong.innerHTML = "Please Send OTP First";
-  } else if (otpInput.value === "") {
-    Wrong.innerHTML = "Please Enter OTP First";
-  } else if (otpInput.value === otpholder) {
-    Wrong.innerHTML = "Welcome";
-    otpholder = ""; // reset
-  } else {
-    Wrong.innerHTML = "OTP is wrong, Try Again";
+    otpInterval = setInterval(generateOtp, 10000);
   }
 
-  otpInput.value = "";
-}
+  function generateOtp() {
+    let otp = "";
+    for (let i = 0; i < 6; i++) {
+      otp += Math.floor(Math.random() * 10);
+    }
+    otp_message.innerHTML = "Your OTP is " + otp;
+    otpholder = otp;
+  }
+
+  function checkotp() {
+    if (otpholder === "") {
+      Wrong.innerHTML = "Please Send OTP First";
+    } else if (otpInput.value === "") {
+      Wrong.innerHTML = "Please Enter OTP First";
+    } else if (otpInput.value === otpholder) {
+      Wrong.innerHTML = "Welcome";
+      Wrong.style.color = "green";
+      otpholder = "";
+    } else {
+      Wrong.innerHTML = "OTP is wrong, Try Again";
+      Wrong.style.color = "red";
+    }
+    otpInput.value = "";
+  }
+
+  // make functions global so HTML buttons can use them
+  window.sentotp = sentotp;
+  window.checkotp = checkotp;
+};
