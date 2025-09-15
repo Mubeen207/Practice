624db56a78889d7d7ee3b877aba44d3f3c80c923
MUBEEN207 <mubeenulislam207@gmail.com>
2025-09-15 04:34:27 -0700

    miore

--- Filter Table/app.js
+++ Filter Table/app.js
@@ -1,5 +1,7 @@
 let myInput = document.getElementById("myInput");
 let myTable = document.getElementById("myTable");
+let name1 = document.getElementById("name");
+let country = document.getElementById("country");
 
 function myFunction() {
   let filter = myInput.value.toLowerCase();
@@ -12,5 +14,28 @@ function myFunction() {
     } else {
       rows[i].style.display = "";
     }
+  }
+}
+function add() {
+  let addSetionTr = document.createElement("tr");
+  let addSetionTd1 = document.createElement("td");
+  let addSetionTd2 = document.createElement("td");
+  if (name1.value === "") {
+    name1.style.border = "2px solid red";
+  } else if (country.value === "") {
+    country.style.border = "2px solid red";
+  } else if (name1.value !== "" && country.value !== "") {
+    myTable.appendChild(addSetionTr);
+    addSetionTr.appendChild(addSetionTd1);
+    addSetionTr.appendChild(addSetionTd2);
+    addSetionTd1.innerHTML = name1.value;
+    addSetionTd2.innerHTML = country.value;
+  }
+}
+function borderReset() {
+  if (name1.value == "") {
+    name1.style.border = "0.5px solid black";
+  } else if (country.value == "") {
+    country.style.border = "0.5px solid black";
   }
 }
