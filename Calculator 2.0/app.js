4f8dae87393cbd800ed3c200a100c536b6857699
MUBEEN207 <mubeenulislam207@gmail.com>
2025-09-22 18:26:22 +0500

    issue

--- Calculator 2.0/app.js
+++ Calculator 2.0/app.js
@@ -1,18 +1,46 @@
 let input = document.getElementById("input");
 let result = "";
+let euation = "";
+let count = true;
 function addinput(num) {
-  result = result + num;
-  input.value = result;
+  euation += num;
+  input.value = euation;
+  count = true;
 }
 function answer() {
-  result = eval(result);
-  input.value = result;
+  if (
+    euation[euation.length - 1] === "+" ||
+    euation[euation.length - 1] === "-" ||
+    euation[euation.length - 1] === "/" ||
+    euation[euation.length - 1] === "*"
+  ) {
+    for (let i = 0; i < euation.length - 1; i++) {
+      result += euation[i];
+    }
+
+    result = eval(result);
+    input.value = result;
+    euation = "";
+  }
+}
+function special(rider) {
+  if (count) {
+    euation += rider;
+    count = false;
+    input.value = euation;
+  }
 }
 function remove() {
   input.value = "";
-  result = "";
+  euation = "";
 }
 function remove1() {
-  result = result.slice(0, -1);
-  input.value = result;
+  euation = euation.slice(0, -1);
+  input.value = euation;
 }
+// let one = "2++2";
+// one = Number(one);
+// // console.log(Number(one));
+// if (isNaN(one)) {
+//   console.log(one, " Hello");
+// }
