404fbfdda235fb5d152b74e0249b0b8b0210cf11
MUBEEN207 <mubeenulislam207@gmail.com>
2025-10-22 17:06:12 +0500

    card game pdate

--- Cards Bet Game/app.js
+++ Cards Bet Game/app.js
@@ -2,18 +2,23 @@ let img1 = document.getElementById("img-1");
 let img2 = document.getElementById("img-2");
 let img3 = document.getElementById("img-3");
 let h1 = document.getElementById("heading");
-let isFlipped = false;
+let buttonAddEl = document.getElementById("button-add");
 let AmountEl = document.getElementById("amount");
 let Input = document.getElementById("input");
+let chanceEl = document.getElementById("chance");
+let resetCardEl = document.getElementById("resetCard");
+let isFlipped = false;
 let result = 0;
 let inputNum = 0;
 let amountNum = 0;
+let balance = 0;
 
 function flipCard(num) {
   inputNum = parseFloat(Input.value);
   amountNum = parseFloat(AmountEl.innerText);
   if (amountNum >= inputNum && inputNum !== String) {
     if (!isFlipped) {
+      chanceEl.innerHTML--;
       let kingNumber = Math.ceil(Math.random() * 3);
 
       img1.src = "./Images/joker.jpg";
@@ -30,10 +35,40 @@ function flipCard(num) {
         h1.innerHTML = "🎉 You Win!";
         h1.style.color = "green";
         Add();
-      } else {
+      } else if (kingNumber !== num) {
         h1.innerHTML = "❌ You Loss!";
         h1.style.color = "red";
         SumTract();
+      }
+      if (chanceEl.innerHTML == 0) {
+        if (AmountEl.innerHTML > balance) {
+          h1.innerHTML =
+            "You Win Profet $ " + (Number(AmountEl.innerHTML) - balance);
+          h1.style.color = "green";
+          balance = 0;
+        } else {
+          h1.innerHTML =
+            "Your Loss is $ " + (Number(AmountEl.innerHTML) - balance);
+          h1.style.color = "red";
+          balance = 0;
+        }
+      } else if (AmountEl.innerHTML == 0) {
+        if (AmountEl.innerHTML > balance) {
+          h1.innerHTML =
+            "You Win Profet $ " + (Number(AmountEl.innerHTML) - balance);
+          h1.style.color = "green";
+          balance = 0;
+        } else {
+          h1.innerHTML =
+            "Your Loss is $ " + (Number(AmountEl.innerHTML) - balance);
+          h1.style.color = "red";
+          balance = 0;
+        }
+        chanceEl.innerHTML = 0;
+      }
+      if (chanceEl.innerHTML == 0) {
+        resetCardEl.setAttribute("onclick", "playAgain()");
+        resetCardEl.innerHTML = "Play Again";
       }
     }
   } else if (amountNum < inputNum) {
@@ -43,6 +78,14 @@ function flipCard(num) {
     h1.innerHTML = "Please Enter Bet Amount!";
     h1.style.color = "red";
   }
+  Input.value = "";
+}
+function playAgain() {
+  resetCardEl.setAttribute("onclick", "resetCard()");
+  resetCardEl.innerHTML = "ResetCard";
+  resetCard();
+  chanceEl.innerHTML = "5";
+  AmountEl.innerHTML = "";
 }
 
 function resetCard() {
@@ -52,15 +95,48 @@ function resetCard() {
   h1.innerHTML = "Pick a card!";
   h1.style.color = "blue";
   isFlipped = false;
+  resetCardEl.setAttribute("onclick", "resetCard()");
+  resetCardEl.innerHTML = "ResetCard";
 }
 
 function Deposit(Element) {
-  if (Input.value > 0 && Input.value !== String) {
+  if (Input.value < 1000) {
+    h1.innerHTML = "Your Deposit Amount Less Then $1000 !";
+    h1.style.color = "red";
+  } else if (Input.value > 0 && Input.value !== String) {
     AmountEl.innerHTML = Input.value;
+    h1.innerHTML = "Pick a card!";
+    h1.style.color = "blue";
+    balance = Number(AmountEl.innerHTML);
 
     Element.innerHTML = "Reset All";
     Element.setAttribute("OnClick", "Reload(this)");
     Input.value = "";
+    let hundredBtn = document.createElement("button");
+    let fiveHundredBtn = document.createElement("button");
+    let thousandBtn = document.createElement("button");
+
+    let hundredBtnTextNode = document.createTextNode("100");
+    let fiveHundredBtnTextNode = document.createTextNode("500");
+    let thousandBtnTextNode = document.createTextNode("1000");
+
+    hundredBtn.appendChild(hundredBtnTextNode);
+    fiveHundredBtn.appendChild(fiveHundredBtnTextNode);
+    thousandBtn.appendChild(thousandBtnTextNode);
+
+    hundredBtn.setAttribute("class", "add_dep_btn");
+    fiveHundredBtn.setAttribute("class", "add_dep_btn");
+    thousandBtn.setAttribute("class", "add_dep_btn");
+
+    hundredBtn.setAttribute("onClick", "bitAmountBtn(100)");
+    fiveHundredBtn.setAttribute("onClick", "bitAmountBtn(500)");
+    thousandBtn.setAttribute("onClick", "bitAmountBtn(1000)");
+
+    // hundredBtn.setAttribute("id", "hello");
+
+    buttonAddEl.appendChild(hundredBtn);
+    buttonAddEl.appendChild(fiveHundredBtn);
+    buttonAddEl.appendChild(thousandBtn);
   } else {
     h1.innerHTML = "Invalid Value Please Enter Number Only!";
     h1.style.color = "red";
@@ -93,4 +169,12 @@ function Reload(Element) {
   Element.setAttribute("OnClick", "Deposit(this)");
   Input.value = "";
   AmountEl.innerHTML = "";
+  chanceEl.innerHTML = "5";
+  buttonAddEl.removeChild(buttonAddEl.childNodes[7]);
+  buttonAddEl.removeChild(buttonAddEl.childNodes[6]);
+  buttonAddEl.removeChild(buttonAddEl.childNodes[5]);
+}
+
+function bitAmountBtn(num) {
+  Input.value = num;
 }
