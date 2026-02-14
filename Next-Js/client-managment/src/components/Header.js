03405d89d2d6f34893b6c76bd85606c584b98d12
mubeen207 <mubeenulislam207@gmail.com>
2026-02-14 15:45:27 +0500

    work for new

---
+++ Next-Js/client-managment/src/components/Header.js
@@ -0,0 +1,24 @@
+import List from "./List";
+
+const Header = () => {
+  return (
+    <>
+      <div className="flex justify-between items-center px-40 py-10 text-white bg-linear-to-r from-blue-600 to-blue-800">
+        <h1 className="font-bold text-2xl">Mubeen</h1>
+        <ul className="flex gap-6 ">
+          <List btnName="Home" route="/" />
+          <List btnName="About Us" route="/about" />
+          <List btnName="About Us" route="/about" />
+          <List btnName="About Us" route="/about" />
+          <List btnName="About Us" route="/about" />
+          <List btnName="About Us" route="/about" />
+        </ul>
+        <div className="flex gap-3">
+          <button>Sign in</button>
+          <button className="border px-4 py-1">Sign Up</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header;
