88157793b25c529f68a5daeb3f75bf8db885182b
mubeen207 <mubeenulislam207@gmail.com>
2026-03-07 12:07:22 +0500

    updates

--- Athantication/app.js
+++ Athantication/app.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: // Enter YOur Api
+  apiKey: // Enter Your Api
   authDomain: "authentication-1548a.firebaseapp.com",
   projectId: "authentication-1548a",
   storageBucket: "authentication-1548a.firebasestorage.app",
