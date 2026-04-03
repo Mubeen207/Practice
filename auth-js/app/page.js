d73c9c02fc3b57d997f43c067b0f62e2af5dcc30
mubeen207 <mubeenulislam207@gmail.com>
2026-04-03 23:41:34 +0500

    logout button

--- auth-js/app/page.js
+++ auth-js/app/page.js
@@ -2,21 +2,22 @@
 import { getServerSession } from "next-auth";
 import { signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
+import LogoutButton from "./components/LogoutButton";
 // import { useEffect } from "react";
 
 export default function Component() {
-  const { data: session} = getServerSession();
+  const { data: session } = getServerSession();
   // useEffect(() => {
-    if (!session) {
-      redirect("/login");
-    }
+  if (!session) {
+    redirect("/login");
+  }
   // }, [session]);
 
   if (!session) return <p>loading...</p>;
   return (
     <>
       <p>Welcome to deshboard</p>
-     
+      <LogoutButton />
     </>
   );
 }
