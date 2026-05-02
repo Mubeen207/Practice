e5cd50dfdb93f9ad80e0a7627d77b05f64211c34
mubeen207 <mubeenulislam207@gmail.com>
2026-05-02 12:10:45 +0500

    updates

--- problem/src/components/themeProvider/theme-provider.tsx
+++ problem/src/components/themeProvider/theme-provider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-// function
+// functio
 export function ThemeProvider({
   children,
   ...props
